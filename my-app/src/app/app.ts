af6d60397eae249c8af94a4fc520e4de3c3fee95
MDSONU22943 <mdsonu22943@gmail.com>
2025-12-27 16:05:55 +0530

    @for lop | Control Flow, track

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -7,13 +7,11 @@ import { Component, signal, WritableSignal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  isLogin = signal(false)
-  show=signal(true)
-  status=signal("notStarted")
-
-  handleLogin(status:boolean){
-    this.isLogin.set(status)
-  }
-
-
+  users = signal(["Anil", "Sam","Peter","Bruce" ])
+  usersDetail = signal([
+    {id:1, name:"Anil", surname:"sidhu", email:"anil@test.com"},
+    {id:2, name:"sam", surname:"singh", email:"sam@test.com"},
+    {id:3, name:"Peter", surname:"parker", email:"peter@test.com"},
+    {id:4, name:"bruce", surname:"wayen", email:"bruce@test.com"},
+  ])
 }
