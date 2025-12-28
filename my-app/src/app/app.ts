1640659c0199128285524f23559ae2b2a0c1ae5e
MDSONU22943 <mdsonu22943@gmail.com>
2025-12-28 17:16:44 +0530

    two way data binding

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,17 +1,24 @@
 import { Component, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  users = signal(["Anil", "Sam","Peter","Bruce" ])
-  usersDetail = signal([
-    {id:1, name:"Anil", surname:"sidhu", email:"anil@test.com"},
-    {id:2, name:"sam", surname:"singh", email:"sam@test.com"},
-    {id:3, name:"Peter", surname:"parker", email:"peter@test.com"},
-    {id:4, name:"bruce", surname:"wayen", email:"bruce@test.com"},
-  ])
+  // name=signal("Anil")
+  // age=20
+
+  userData = signal({
+    name:"anil sidhu",
+    age:25,
+    email:"anil@test.com"
+  })
+
+  updateUserData(key:string, val:string){
+    this.userData.update((item)=>({...item, name:val}))
+  }
+
 }
