58030d23efbb7a8b3f32e6a6895dd9f0ae872b4f
MDSONU22943 <mdsonu22943@gmail.com>
2026-01-01 17:14:19 +0530

    Parent to child data passing | @input Decorator

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,23 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal, WritableSignal } from '@angular/core';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, Child],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
+  // userName = signal("anil sidhu")
+  userName = signal(['anil', 'peter','sam','bruce'])
+  newUser = signal('')
 
-  students = ["Anil", "sam", "peter", "vinay", "bruce"]
+  addNewUser(){
+    this.userName.update((data)=> ([...data, this.newUser()]))
+  }
 
-  studentsData = [
-    {name:"Anil", age:29, email:"anil@test.com"},
-    {name:"vinay", age:30, email:"anil2@test.com"},
-    {name:"bruce", age:31, email:"anil3@test.com"},
-    {name:"peter", age:32, email:"anil4@test.com"},
-  ]
-
-  show=true
 }
