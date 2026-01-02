81e46ac5ef74e7114573e046aa6adcc38e79a818
MDSONU22943 <mdsonu22943@gmail.com>
2026-01-02 23:17:07 +0530

    Pipes | Uppercase, Date, Currency, JSON pipe

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal, WritableSignal } from '@angular/core';
-import { DisplayCount } from './display-count/display-count';
-import { ControlCount } from './control-count/control-count';
+
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, DisplayCount, ControlCount],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
+  title="code step by step"
+  name = signal('')
 
+  amount = 10000
+  date ="24-8-2025"
+  mobile="samsung"
+
+  user = signal({name:'anil', age:29, email:"code@test.com"})
 }
