69781fedcd221ebf01389a58d8bebc4e912e6d32
MDSONU22943 <mdsonu22943@gmail.com>
2026-01-05 20:54:54 +0530

    Query Parameters | Pass Read data using url

--- my-app/src/app/pages/home/home.ts
+++ my-app/src/app/pages/home/home.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-  constructor(public router:Router){}
+  constructor(public route:Router){}
+  userData = signal({name:"anil",age:29,id:1})
+
   goToProfile(){
-    this.router.navigate(['profile'])
+    this.route.navigate(['profile'], {
+      queryParams:{
+        id:10,
+        name:'peter',
+        age:40
+      }
+    })
   }
+
 }
--- my-app/src/app/pages/profile/profile.ts
+++ my-app/src/app/pages/profile/profile.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -8,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class Profile {
 
+  name=signal('')
+  id=signal(0)
+  age=signal(0)
+  constructor(public route:ActivatedRoute){}
+
+  ngOnInit(){
+    this.route.queryParams.subscribe((params)=>{
+      console.log(params)
+      this.name.set(params['name'])
+      this.id.set(params['id'])
+      this.age.set(params['age'])
+    })
+  }
 }
