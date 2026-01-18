58ea8a70e9e2aa0247775884ee1f037cd7bc93d2
MDSONU22943 <mdsonu22943@gmail.com>
2026-01-18 14:38:29 +0530

    angular material UI setup done

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-
-import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, MatButtonModule,MatDividerModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
